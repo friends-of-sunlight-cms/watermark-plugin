b8c3b862c5e287aa40bd638aad24bce8010cbc7b
jDanek <jirka.danek@jdanek.eu>
2024-02-03 18:18:57 +0100

    [WIP] Update compatibility for SL 8.1

--- plugins/extend/watermark/class/ConfigAction.php
+++ plugins/extend/watermark/class/ConfigAction.php
@@ -4,34 +4,33 @@ namespace SunlightExtend\Watermark;
 
 use Sunlight\Plugin\Action\ConfigAction as BaseConfigAction;
 use Sunlight\Util\Form;
+use Sunlight\Util\Request;
 
 class ConfigAction extends BaseConfigAction
 {
     protected function getFields(): array
     {
         $config = $this->plugin->getConfig();
-        $positions = Watermark::getPositions();
+        $position = $config['watermark_position'] ?? Watermark::POS_CENTER;
+        $positions = [];
+        foreach (Watermark::getPositions() as $pos) {
+            $positions[$pos] = _lang('watermark.config.pos.' . $pos);
+        }
 
         return [
             'watermark_file' => [
                 'label' => _lang('watermark.config.watermark_file'),
-                'input' => '<input type="text" name="config[watermark_file]" ' . Form::restorePostValue('config[watermark_file]', $config['watermark_file']) . ' class="inputmedium">',
+                'input' => '<input type="text" name="config[watermark_file]" ' . Request::post('config[watermark_file]', $config['watermark_file']) . ' class="inputmedium">',
                 'type' => 'text',
             ],
             'watermark_position' => [
                 'label' => _lang('watermark.config.watermark_position'),
-                'input' => _buffer(function () use ($config, $positions) { ?>
-                    <select name="config[watermark_position]" class="inputmedium">
-                        <?php foreach ($positions as $position) : ?>
-                            <option value="<?= $position ?>"<?= $config['watermark_position'] == $position ? ' selected' : '' ?>><?= _lang('watermark.config.pos.' . $position) ?></option>
-                        <?php endforeach; ?>
-                    </select>
-                <?php }),
+                'input' => Form::select('config[watermark_position]', $positions, $position, ['class' => 'inputmedium']),
                 'type' => 'text',
             ],
             'resize_large_watermark' => [
                 'label' => _lang('watermark.config.resize_large_watermark'),
-                'input' => '<input type="checkbox" name="config[resize_large_watermark]" ' . Form::restorePostValue('config[resize_large_watermark]', $config['resize_large_watermark']) . '>',
+                'input' => '<input type="checkbox" name="config[resize_large_watermark]" ' . Request::post('config[resize_large_watermark]', $config['resize_large_watermark']) . '>',
                 'type' => 'checkbox',
             ]
         ];
